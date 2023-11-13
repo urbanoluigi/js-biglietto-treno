9254f568bedbd100dd1dca8ed4aa8ac4e2f13c0d
urbanoluigi <urrbanoluigi@gmail.com>
2023-11-13 15:42:24 +0100

    maybe finish

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Chiedi all'utente il numero di chilometri da percorrere e l'età
+let kmDaPercorrere = parseFloat(prompt("Inserisci il numero di chilometri da percorrere:"));
+let etaPasseggero = parseInt(prompt("Inserisci la tua età:"));
+
+// Calcola il prezzo del biglietto
+let prezzoBase = kmDaPercorrere * 0.21;
+
+// Applica gli sconti in base all'età
+if (etaPasseggero < 18) {
+    prezzoBase -= prezzoBase * 0.2; // Sconto del 20% per i minorenni
+} else if (etaPasseggero >= 65) {
+    prezzoBase -= prezzoBase * 0.4; // Sconto del 40% per gli over 65
+}
+
+// Formatta il prezzo finale con massimo due decimali
+let prezzoFinale = prezzoBase.toFixed(2);
+
+// Visualizza il risultato nel documento HTML
+let risultatoElement = document.createElement("p");
+risultatoElement.textContent = "Il prezzo del biglietto è: " + prezzoFinale + " €";
+document.body.appendChild(risultatoElement);
